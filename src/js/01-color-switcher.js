747ab99d2f551dc4bebc5746055e553833ddadbc
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-21 21:36:21 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let colorChangeInterval;
+
+function startColorChanging() {
+  colorChangeInterval = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  document.querySelector('[data-start]').disabled = true;
+}
+
+function stopColorChanging() {
+  clearInterval(colorChangeInterval);
+  document.querySelector('[data-start]').disabled = false;
+}
+
+document.querySelector('[data-start]').addEventListener('click', startColorChanging);
+document.querySelector('[data-stop]').addEventListener('click', stopColorChanging);
