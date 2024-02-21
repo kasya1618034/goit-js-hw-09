7f89bf5c7e57932fa965279531becf945f81b7c3
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-21 22:38:38 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,3 +19,21 @@ const options = {
 };
 
 flatpickr("#datetime-picker", options);
+
+let countdownInterval;
+
+function startCountdown() {
+  const selectedDate = flatpickr.parseDate(document.querySelector('#datetime-picker').value, 'Y-m-d H:i');;
+  const currentDate = new Date();
+  if (selectedDate <= currentDate) {
+    window.alert("Please choose a date in the future");
+    return;
+  }
+
+  countdownInterval = setInterval(updateCountdown, 1000);
+  updateCountdown();
+}
+
+function updateCountdown() {
+
+}
