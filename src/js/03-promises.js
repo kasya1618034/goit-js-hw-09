fb108e58489e10baba8fe8a48ee1629ccc2b1d4a
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-22 19:44:59 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,17 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve({ position, delay });
-  } else {
-    reject({ position, delay });
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 document.querySelector('.form').addEventListener('submit', function (event) {
   event.preventDefault();
 });
+
