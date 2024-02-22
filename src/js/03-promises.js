b03812792a7067815deee6832d968428c6073633
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-22 20:02:04 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,5 +13,21 @@ function createPromise(position, delay) {
 
 document.querySelector('.form').addEventListener('submit', function (event) {
   event.preventDefault();
+
+  const delay = parseInt(this.elements.delay.value);
+  const step = parseInt(this.elements.step.value);
+  const amount = parseInt(this.elements.amount.value);
+
+  let currentDelay = delay;
+  for (let i = 1; i < amount; i += 1) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    currentDelay += step;
+  }
 });
 
