60990cb43cdfa1bced2599a4eaf1da2f89ecbb1f
kasya1618034 <86284324+kasya1618034@users.noreply.github.com>
2024-02-22 19:02:28 +0000

    Deploying to gh-pages from @ kasya1618034/goit-js-hw-09@b03812792a7067815deee6832d968428c6073633 🚀

--- 03-promises.a97a0147.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=03-promises.a97a0147.js.map
--- 03-promises.bdfe6ca2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=03-promises.bdfe6ca2.js.map
